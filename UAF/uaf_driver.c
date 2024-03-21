9fe53beca636ebbcc2d995f825bcf566d0dfa798
Your Name <you@example.com>
2024-03-21 16:23:35 -0700

    Test from VM

--- UAF/uaf_driver.c
+++ UAF/uaf_driver.c
@@ -1,20 +1,31 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/printk.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/phantom.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jared B");
-MODULE_DESCRIPTION("Vulnerable UAF Module");
+MODULE_DESCRIPTION("Vulnerable UAF Driver :)");
 
-int uaf_init(void) 
+// static ssize_t uaf_read(struct file* file, char __user *, 
+
+static void set_file_operations(void) {
+}
+
+static int __init uaf_init(void) 
 {
-	printk("uaf init");
+	set_file_operations();
+
 	return 0;
 }
 
-void uaf_exit(void) 
+static void __exit uaf_exit(void) 
 {
-	printk("uaf exit");
+	pr_debug("goodbye, exit\n");
 }
 
 module_init(uaf_init);
