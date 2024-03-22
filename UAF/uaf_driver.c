1b6fe52415c497f3d59ea5a22f2bd105559c5a58
Your Name <you@example.com>
2024-03-22 10:35:18 -0700

    Developing Misc Device

--- UAF/uaf_driver.c
+++ UAF/uaf_driver.c
@@ -1,29 +1,42 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/slab.h>
-#include <linux/printk.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/phantom.h>
+#include <linux/miscdevice.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Jared B");
+MODULE_AUTHOR("Jared");
 MODULE_DESCRIPTION("Vulnerable UAF Driver :)");
 
-// static ssize_t uaf_read(struct file* file, char __user *, 
 
-static void set_file_operations(void) {
+static struct miscdevice
+initialize_vuln_misc_driver(void)
+{
+	struct miscdevice m = { 
+		.minor = MISC_DYNAMIC_MINOR, 
+		.name  			= "Vulnerable UAF Driver",
+		.fops  			= NULL, 
+		.list 			= {},
+		.parent 		= NULL,
+		.this_device 	= NULL,
+		.groups 	 	= NULL,
+		.nodename  		= "Awesome UAF Vuln", 
+		.mode  			= 1, 
+	};
+
+	return m;
 }
 
-static int __init uaf_init(void) 
+static int __init
+uaf_init(void) 
 {
-	set_file_operations();
+	struct miscdevice m = initialize_vuln_misc_driver();
+	misc_register(&m);	
 
 	return 0;
 }
 
-static void __exit uaf_exit(void) 
+static void __exit
+uaf_exit(void) 
 {
 	pr_debug("goodbye, exit\n");
 }
